d846bdabea07b28e8bd65a02e03fd77c295b7717
Satrajit Maitra <satrajitmaitra@Satrajits-MacBook-Pro.local>
2018-10-17 01:07:05 -0700

    Commiting changes to git

---
+++ knn.py
@@ -0,0 +1,44 @@
+from __future__ import division
+import scipy
+import numpy as np
+import load_dataset as ld
+import matplotlib.pyplot as plt
+from scipy.spatial import distance
+
+
+train_data = ld.read()
+y_train_data = train_data[0]
+x_train_data = train_data[1].reshape([60000, 28 * 28])
+
+test_data = ld.read('testing')
+y_test_data = test_data[0]
+x_test_data = test_data[1].reshape([10000, 28 * 28])
+
+distance_list = distance.cdist(x_test_data,x_train_data)
+
+def most_common(list):
+    return max(set(list), key = list.count)
+
+def knn(k):
+    count = 0
+    for i in range(len(distance_list)):
+        if most_common(list(y_train_data[np.argpartition(distance_list[i],k)[:k]])) == y_test_data[i]:
+            count += 1
+
+    accuracy = count / 10000
+    accuracy_list.append(accuracy)
+    print("For k = ", str(k), " accuracy ", accuracy)
+    return accuracy
+
+def main():
+    k_list = [1, 3, 5, 10, 30, 50, 70, 80, 90, 100]
+    accuracy_list=[]
+    for k in k_list:
+        accuracy_list.append(knn(k))
+
+    plt.plot(k_list, list(np.array(accuracy_list)*100), "ro")
+    plt.xlabel("K")
+    plt.ylabel("Accuracy")
+
+if __name__ == "__main__":
+    main()
---
+++ logistic_regression.py
@@ -0,0 +1,75 @@
+from __future__ import division
+import scipy
+import numpy as np
+import load_dataset as ld
+import matplotlib.pyplot as plt
+
+train_data = ld.read()
+alpha = 0.001
+y_train_data = train_data[0]
+x_train_data = train_data[1].reshape([60000, 28 * 28]) / 255
+
+test_data = ld.read('testing')
+y_test_data = test_data[0]
+x_test_data = test_data[1].reshape([10000, 28 * 28]) / 255
+
+weights = np.tile(np.zeros(784), (9, 1))
+
+def post_prob(x, y):
+    if y == 9:
+        return 1 / sum_exps(x)
+    else:
+        return np.exp(np.dot(weights[y], x)) / sum_exps(x)
+
+def sum_exps(x):
+    exp_array = []
+    for weight in weights:
+        exp_array.append(np.exp(np.dot(weight, x)))
+    return (1 + np.sum(exp_array))
+
+def gradient_ascent(p, y, x):
+    for i in range(len(weights)):
+        if y == i:
+            weights[i] = np.add(np.array(weights[i]), np.array(alpha * (1 - p[i]) * np.array(x)))
+        else:
+            weights[i] = np.add(np.array(weights[i]), np.array(alpha * (-1) * p[i] * np.array(x)))
+
+def train():
+    for i in range(len(x_train_data)):
+        predictions = []
+        for j in range(10):
+            predictions.append(round(post_prob(x_train_data[i], j), 2))
+        gradient_ascent(predictions, y_train_data[i], x_train_data[i])
+
+
+def test():
+    predictions = []
+    for i in range(len(x_test_data)):
+        p = []
+        for j in range(10):
+            p.append(round(post_prob(x_test_data[i], j), 2))
+        index = np.argmax(p)
+        predictions.append(index)
+    count = 0
+    for i in range(len(predictions)):
+        if predictions[i] == y_test_data[i]:
+            count += 1
+    return (count / 100)
+
+def main():
+    accuracy_list = []
+    size_list = []
+    for i in range(100):
+        print("ITERATION", str(i))
+        print("TRAINING")
+        train()
+        print("TESTING")
+        accuracy = test()
+        accuracy_list.append(accuracy)
+        size_list.append((i+1))
+
+    plt.plot(size_list, accuracy_list, 'ro')
+    plt.show()
+
+if __name__ == "__main__":
+    main()
